9729020ba6e556a99c9affc260e5dc0e2209024c
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-01-17 23:14:55 -0300

    Styling exaple

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -5,5 +5,9 @@ interface HabitProps {
 }
 
 export function Habit(props: HabitProps) {
-  return <p className="bg-red-800">{props.completed}</p>;
+  return (
+    <p className="bg-zinc-800 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
+      {props.completed}
+    </p>
+  );
 }
